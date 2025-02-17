ab99f33b4c6ec52c3a85b00e6bd408c13fa19267
Leo Irakliotis <lgreco@gmail.com>
2025-02-17 20:54:55 +0000

    dag

---
+++ Graphs/DirectedAcyclicGraph.java
@@ -0,0 +1,16 @@
+public class DirectedAcyclicGraph {
+
+    public static void main(String[] args) {
+
+        int __ = Integer.MAX_VALUE;
+
+        int[][] G = {
+                { __, __, __, 2, 20, __ },
+                { __, __, __, __, __, 2 },
+                { __, 6, __, __, __, __ },
+                { __, 5, 4, __, 25, 15 },
+                { __, __, __, __, __, __ },
+                { __, __, __, __, 6, __ }
+        };
+    } // method main
+} // class DirectedAcyclicGraph
